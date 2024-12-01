c59ae88622aa5ac82224a73ffc6dde80d7d6f2d5
veysel.gunes <veysel.gunes36@gmail.com>
2024-12-01 23:00:53 +0300

    add todo app first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import Grid from "@mui/material/Grid";
+import { Box, Button } from "@mui/material";
+import AddTodoItemModal from "./components/AddTodoItemModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+  const [todoItems, setTodoItems] = useState([]);
+
+  // Add todo item function here
+
+  const addTodoItem = (newItem) => {
+    // Add todo item logic here
+    console.log("Add Todo Item", newItem);
+    setTodoItems([...todoItems, newItem]);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>TODO LİST </h2>
+      <Box sx={{ flexGrow: 2 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={0} md={3} lg={3}></Grid>
+          <Grid item xs={12} md={6} lg={6}>
+            <TodoForm>xs=8</TodoForm>
+            <AddTodoItemModal
+              open={open}
+              handleClose={handleClose}
+              addTodoItem={addTodoItem}
+            ></AddTodoItemModal>
+            <TodoList addTodoItem={addTodoItem} todoItems={todoItems} />
+          </Grid>
+          <Grid item xs={0} md={3} lg={3}>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              Slide in alert dialog
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AddTodoItemModal.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  TextField,
+} from "@mui/material";
+const AddTodoItemModal = ({ open, handleClose, addTodoItem }) => {
+  const [title, setTitle] = useState("");
+
+  const appleTodoItem = () => {
+    addTodoItem({ title });
+    setTitle("");
+    handleClose();
+  };
+  return (
+    <div>
+      <Dialog
+        open={open}
+        xs={{ w: "1000px", height: "auto" }}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"New Note"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description"></DialogContentText>
+          <TextField
+            id="outlined-basic"
+            hiddenLabel
+            variant="outlined"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button sx={{ float: "right" }} onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button sx={{ float: "left" }} onClick={appleTodoItem}>
+            Appley
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddTodoItemModal;
